023005120e9eb7ed5bb8c2e471d66fc446d4b363
Maximilian Ammann <max@maxammann.org>
2020-07-08 17:18:01 +0200

    NATIVE-552: Update scripts

--- external-jobs/2020-06-malteser/to-csv.sh
+++ external-jobs/2020-06-malteser/to-csv.sh
@@ -2,7 +2,6 @@
 
 function to_csv {
   libreoffice --headless --infilter=CSV:44,34,76,1  --convert-to csv --outdir $1 $1/*.ods
-  rm $1/*.ods
 }
 
 to_csv ./external-jobs/2020-06-malteser/sent/native
--- external-jobs/2020-06-malteser/to-ods.sh
+++ external-jobs/2020-06-malteser/to-ods.sh
@@ -3,7 +3,6 @@
 # Interprets the CSVs as UTF-8 and converts to ods
 function to_ods {
   libreoffice --headless --infilter=CSV:44,34,76,1 --convert-to ods --outdir $1 $1/*.csv
-  rm $1/*.csv
 }
 
 to_ods ./external-jobs/2020-06-malteser/sent/native
