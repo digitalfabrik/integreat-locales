a1386ce4c047f373d001ebbd7e88b6f03cf7a524
Steffen Kleinle <klinzo@yahoo.de>
2020-08-18 11:51:55 +0200

    NATIVE-552: Make scripts generic and adjust README.md

--- external-jobs/2020-06-malte/to-csv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function to_csv {
-  libreoffice --headless --infilter=CSV:44,34,76,1  --convert-to csv --outdir $1 $1/*.ods
-}
-
-to_csv ./external-jobs/2020-06-malteser/sent/native
-to_csv ./external-jobs/2020-06-malteser/sent/malte-overrides
-to_csv ./external-jobs/2020-06-malteser/received
